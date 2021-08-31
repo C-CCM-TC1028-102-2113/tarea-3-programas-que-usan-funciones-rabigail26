9ce4e671fd9a60c1faa1556348e6fe1567c0c274
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-08-30 19:29:13 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,22 @@
 
+def maximo(pliegos,plumones):
+    tarjetaxpliego = pliegos * 12
+    tarjetasxplumon = plumones * 35
+    if tarjetaxpliego > tarjetasxplumon:
+        total = tarjetasxplumon
+    if tarjetaxpliego < tarjetasxplumon:
+        total = tarjetaxpliego
+    return total
 def main():
     #escribe tu código abajo de esta línea
     pass
+#Tarjetería Española
+
+
+pliegos = int (input ("Dame la cantidad de pliegos de papel albanene: "))
+plumones = int (input ("Dame la cantidad de plumones: "))
+tarjetas = maximo(pliegos,plumones)
+print ("El número máximo de tarjetas que se pueden hacer es: ", int(tarjetas))
 
 if __name__=='__main__':
     main()
